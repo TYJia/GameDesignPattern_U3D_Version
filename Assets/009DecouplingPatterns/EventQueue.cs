a7c0c1a2578106f71e9ed548a18213cff5e0df34
Tianyuan JIA <jty@meitu.com>
2018-07-12 16:30:35 +0800

    解耦模式

---
+++ Assets/009DecouplingPatterns/EventQueue.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventQueue : MonoBehaviour
+{
+    public GameObject PosIndicator;
+    private Queue<Transform> mDestinationQueue;
+    private Transform Destination;
+    public float Speed = 1;
+
+    void Start()
+    {
+        MouseInputManager.Instance.OnMouseClick += AddDestination;
+        mDestinationQueue = new Queue<Transform>();
+    }
+
+    private void AddDestination()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Vector3 destination = ray.GetPoint(10);
+        GameObject desIndi = GameObject.Instantiate(PosIndicator, destination, Quaternion.identity);
+        mDestinationQueue.Enqueue(desIndi.transform);
+    }
+
+    void Update()
+    {
+        if (Destination == null && mDestinationQueue.Count > 0)
+        {
+            Destination = mDestinationQueue.Dequeue();
+        }
+        if (Destination != null)
+        {
+            float distance = Vector3.Distance(transform.position, Destination.position);
+            transform.position = Vector3.Lerp(transform.position, Destination.position,Mathf.Clamp01(Time.deltaTime * Speed / distance));
+            if (distance < 0.01f)
+            {
+                Destroy(Destination.gameObject);
+            }
+        }
+    }
+}
+
---
+++ Assets/009DecouplingPatterns/MouseInputManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseInputManager : MonoBehaviour
+{
+    public delegate void MouseInputeHandler();
+    public MouseInputeHandler OnMouseClick;
+    public static MouseInputManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            OnMouseClick();
+        }
+    }
+}
