ffb42b1ad729af380e40da6f402dd399cdf27a20
Tianyuan JIA <jty@meitu.com>
2018-07-13 19:14:01 +0800

    完结

---
+++ Assets/010OptimizationPatterns/ObjectPool/EventQueueObjPoolVersion.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventQueueObjPoolVersion : MonoBehaviour
+{
+    public GameObject PosIndicator;
+    private Queue<Transform> mDestinationQueue;
+    private Transform Destination;
+    public float Speed = 1;
+    private ObjPool mPool = new ObjPool(); 
+
+    void Start()
+    {
+        MouseInputManager.Instance.OnMouseClick += AddDestination;
+        mDestinationQueue = new Queue<Transform>();
+    }
+
+    private void AddDestination()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Vector3 destination = ray.GetPoint(10);
+
+        GameObject desIndi = (GameObject)mPool.GetObj();
+        if (desIndi == null)
+        {
+            desIndi = GameObject.Instantiate(PosIndicator);
+            mPool.AddObj(desIndi);
+        }
+
+        desIndi.transform.position = destination;
+        mDestinationQueue.Enqueue(desIndi.transform);
+    }
+
+    void Update()
+    {
+        if (Destination == null && mDestinationQueue.Count > 0)
+        {
+            Destination = mDestinationQueue.Dequeue();
+        }
+        if (Destination != null)
+        {
+            float distance = Vector3.Distance(transform.position, Destination.position);
+            transform.position = Vector3.Lerp(transform.position, Destination.position,Mathf.Clamp01(Time.deltaTime * Speed / distance));
+            if (distance < 0.01f)
+            {
+                mPool.DisableObj(Destination.gameObject);
+                Destination = null;
+            }
+        }
+    }
+}
+
---
+++ Assets/010OptimizationPatterns/ObjectPool/ObjPool.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjPool {
+
+    public List<GameObject> ActiveObjs;
+    public List<GameObject> InactiveObjs;
+
+    public ObjPool()
+    {
+        ActiveObjs = new List<GameObject>();
+        InactiveObjs = new List<GameObject>();
+    }
+
+    public void AddObj(GameObject obj)
+    {
+        ActiveObjs.Add(obj);
+    }
+
+    public object GetObj()
+    {
+        if (InactiveObjs.Count > 0)
+        {
+            GameObject obj = InactiveObjs[0];
+            ActiveObjs.Add(obj);
+            InactiveObjs.Remove(obj);
+            obj.SetActive(true);
+            return obj;
+        }
+        return null;
+    }
+
+    public bool DisableObj(GameObject obj)
+    {
+        if (ActiveObjs.Contains(obj))
+        {
+            obj.SetActive(false);
+            ActiveObjs.Remove(obj);
+            InactiveObjs.Add(obj);
+            return true;
+        }
+        return false;
+    }
+
+    public bool RemoveObj(GameObject obj)
+    {
+        if (ActiveObjs.Contains(obj))
+        {
+            ActiveObjs.Remove(obj);
+            GameObject.Destroy(obj);
+            return true;
+        }
+        else if (InactiveObjs.Contains(obj))
+        {
+            InactiveObjs.Remove(obj);
+            GameObject.Destroy(obj);
+            return true;
+        }
+        return false;
+    }
+
+    public void CleanPool()
+    {
+        for (int i = 0; i < ActiveObjs.Count; i++)
+        {
+            GameObject.Destroy(ActiveObjs[i]);
+        }
+        for (int i = 0; i < ActiveObjs.Count; i++)
+        {
+            GameObject.Destroy(InactiveObjs[i]);
+        }
+        ActiveObjs = new List<GameObject>();
+        InactiveObjs = new List<GameObject>();
+    }
+}
